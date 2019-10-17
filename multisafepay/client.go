8c9c5e5d27dead387288d1dec92195163dd5c024
Kurt Stolle <kurt@casualbananas.com>
2019-10-17 12:21:02 +0200

    Improved error handling

--- multisafepay/client.go
+++ multisafepay/client.go
@@ -3,7 +3,9 @@ package multisafepay
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
 )
 
@@ -27,14 +29,8 @@ func NewClient(url, apiKey string) *Client {
 	}
 }
 
-// Get will perform a GET-request to the API
-func (c *Client) Get(route string) (*http.Response, error) {
-	// Prepare HTTP request
-	req, err := http.NewRequest("GET", c.url+route, nil)
-	if err != nil {
-		return nil, err
-	}
-
+// performRequests executes a http.Request and returns the body or an error
+func (c *Client) performRequest(req *http.Request) (io.ReadCloser, error) {
 	// There is a bug in the MultiSafePay API where "api_key" must be lowercase.
 	// Setting the Header map directly is a workaround for this issue.
 	req.Header["api_key"] = []string{c.apiKey}
@@ -45,11 +41,39 @@ func (c *Client) Get(route string) (*http.Response, error) {
 		return nil, err
 	}
 
-	return res, nil
+	// Check status, anything but 200 (OK) is reason to raise an error
+	if res.StatusCode != http.StatusOK {
+		// Find error in the response body
+		var errData ErrorResponse
+		if err := json.NewDecoder(res.Body).Decode(errData); err != nil {
+			return nil, errors.New(
+				fmt.Sprintf("api responded with http status %d %s, but the error could not be decoded from the response body: %s",
+					res.StatusCode,
+					res.Status,
+					err.Error()))
+		}
+
+		return nil, errors.New(fmt.Sprintf("api responded with error: %s (code %d)", errData.ErrorInfo, errData.ErrorCode))
+	}
+
+	// Return the response body
+	return res.Body, nil
+}
+
+// Get will perform a GET-request to the API
+func (c *Client) Get(route string) (io.ReadCloser, error) {
+	// Prepare HTTP request
+	req, err := http.NewRequest("GET", c.url+route, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// Do the request over the default client
+	return c.performRequest(req)
 }
 
 // Post will perform a POST-request to the API with JSON-encoded data
-func (c *Client) Post(route string, data interface{}) (*http.Response, error) {
+func (c *Client) Post(route string, data interface{}) (io.ReadCloser, error) {
 	// Encode data
 	var buffer bytes.Buffer
 	if err := json.NewEncoder(&buffer).Encode(data); err != nil {
@@ -62,20 +86,10 @@ func (c *Client) Post(route string, data interface{}) (*http.Response, error) {
 		return nil, err
 	}
 
-	// There is a bug in the MultiSafePay API where "api_key" must be lowercase.
-	// Setting the Header map directly is a workaround for this issue.
-	req.Header["api_key"] = []string{c.apiKey}
-
 	// Content-Type header ensures the API reads our data as JSON
 	req.Header.Set("Content-Type", "application/json")
 
-	// Do the request over the default client
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	return res, nil
+	return c.performRequest(req)
 }
 
 // CreateOrder creates a new order
@@ -85,11 +99,11 @@ func (c *Client) CreateOrder(o Order) (*PostOrderResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
+	defer res.Close()
 
 	// Decode JSON
 	data := &PostOrderResponse{}
-	if err := json.NewDecoder(res.Body).Decode(data); err != nil {
+	if err := json.NewDecoder(res).Decode(data); err != nil {
 		return nil, err
 	}
 
@@ -103,11 +117,11 @@ func (c *Client) GetOrder(orderID string) (*GetOrderResponse, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer res.Body.Close()
+	defer res.Close()
 
 	// Decode JSON
 	data := &GetOrderResponse{}
-	if err := json.NewDecoder(res.Body).Decode(data); err != nil {
+	if err := json.NewDecoder(res).Decode(data); err != nil {
 		return nil, err
 	}
 
